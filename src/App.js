2a1714d4d3cb735f5b3ddfd0aae820c61a5f57aa
sekharg62 <ghoshsekhar6296@gmail.com>
2024-04-05 05:55:58 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+//import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+import Product from './Pages/Product';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/men' element={<ShopCategory category="men" banner={men_banner} />} />
+          <Route path='/women' element={<ShopCategory category="women" banner={women_banner} />} />
+          <Route path='/kids' element={<ShopCategory category="kid" banner={kids_banner} />} />
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>} />
+
+          </Route>
+           
+         
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
